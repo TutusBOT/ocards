e9ee58859ff390f80717477468792ef29814405c
TutusBOT <barte77pl@gmail.com>
2022-12-04 23:56:41 +0100

    allow user to practice by writing with visual feedback

--- src/components/Practice/Practice.tsx
+++ src/components/Practice/Practice.tsx
@@ -12,12 +12,13 @@ import {
 	Typography,
 } from "@mui/material";
 import { TransitionProps } from "@mui/material/transitions";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { FlashCard, Set } from "../../redux/cards/cardsSlice";
 import CloseIcon from "@mui/icons-material/Close";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import { practiceActions } from "../../redux/practice/practiceSlice";
 
 const Transition = React.forwardRef(function Transition(
 	props: TransitionProps & {
@@ -33,15 +34,28 @@ interface Practice {
 }
 
 const Practice = ({ setName }: Practice) => {
+	const dispatch = useDispatch();
 	const [open, setOpen] = useState(false);
+	const [showAnswer, setShowAnswer] = useState(false);
+	const [answer, setAnswer] = useState("");
+	const [isAnswered, setIsAnswered] = useState(false);
+	const [isAnswerCorrect, setIsAnswerCorrect] = useState<null | boolean>(null);
 	const [set] = useSelector((state: RootState) =>
 		state.persistedReducer.cards.sets.filter((set) => set.name === setName)
 	);
-	const [reviewSet, setReviewSet] = useState<FlashCard[]>(
-		set.cards.sort((a, b) => 0.5 - Math.random())
+	const reviewSet = useSelector(
+		(state: RootState) => state.persistedReducer.practice.cards
 	);
-	const [guess, setGuess] = useState("");
-	const [showAnswer, setShowAnswer] = useState(false);
+
+	useEffect(() => {
+		if (!reviewSet.length) {
+			dispatch(
+				practiceActions.setPractice(
+					set.cards.sort((a, b) => 0.5 - Math.random())
+				)
+			);
+		}
+	}, []);
 
 	const handleClose = () => {
 		setOpen(false);
@@ -55,7 +69,27 @@ const Practice = ({ setName }: Practice) => {
 		setShowAnswer(!showAnswer);
 	};
 
-	const handleGuess = () => {};
+	const handleGuess = () => {
+		setIsAnswered(true);
+		setShowAnswer(true);
+		if (answer === reviewSet[0].back) {
+			setIsAnswerCorrect(true);
+		} else {
+			setIsAnswerCorrect(false);
+		}
+	};
+
+	const handleSetAsCorrect = () => {
+		setIsAnswerCorrect(true);
+	};
+
+	const handleNextAnswer = () => {
+		dispatch(practiceActions.setPractice(reviewSet.slice(1)));
+		setIsAnswered(false);
+		setAnswer("");
+		setShowAnswer(false);
+		setIsAnswerCorrect(null);
+	};
 
 	return (
 		<>
@@ -74,27 +108,60 @@ const Practice = ({ setName }: Practice) => {
 					</Toolbar>
 				</AppBar>
 				<Card className=" h-full bg-purple-900 m-4">
-					<Grid container spacing={4}>
+					<Grid
+						container
+						spacing={4}
+						justifyContent="center"
+						direction="column"
+						alignContent="center"
+					>
 						<Grid item sm={12}>
-							<Typography variant="h4">{reviewSet[0].front}</Typography>
+							<Typography variant="h4" align="center">
+								{reviewSet[0].front}
+							</Typography>
 						</Grid>
-						<Grid item sm={12} className={showAnswer ? "" : "hidden"}>
-							<Typography>{reviewSet[0].back}</Typography>
+						<Grid
+							item
+							sm={12}
+							className={!showAnswer ? "opacity-0 select-none" : ""}
+						>
+							<Typography align="center">{reviewSet[0].back}</Typography>
 						</Grid>
 						<Grid item sm={12}>
 							<TextField
 								variant="outlined"
-								value={guess}
-								onChange={(e) => setGuess(e.target.value)}
+								value={answer}
+								onChange={(e) => setAnswer(e.target.value)}
+								focused={isAnswered}
+								color={
+									isAnswerCorrect !== null
+										? isAnswerCorrect
+											? "success"
+											: "error"
+										: "primary"
+								}
 							/>
 						</Grid>
 						<Grid item sm={12}>
-							<Button variant="outlined" onClick={handleShowAnswer}>
-								SHOW ANSWER
-							</Button>
-							<Button variant="contained" onClick={handleGuess}>
-								SUBMIT
-							</Button>
+							{!isAnswered ? (
+								<>
+									<Button variant="outlined" onClick={handleShowAnswer}>
+										SHOW ANSWER
+									</Button>
+									<Button variant="contained" onClick={handleGuess}>
+										SUBMIT
+									</Button>
+								</>
+							) : (
+								<>
+									<Button variant="outlined" onClick={handleSetAsCorrect}>
+										SET AS CORRECT
+									</Button>
+									<Button variant="contained" onClick={handleNextAnswer}>
+										NEXT
+									</Button>
+								</>
+							)}
 						</Grid>
 					</Grid>
 				</Card>
