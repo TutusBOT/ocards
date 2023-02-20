cf1c05c137740f56afb4e9a404ca2841e7da9e75
TutusBOT <barte77pl@gmail.com>
2023-02-20 23:53:41 +0100

    added new section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,28 +2,26 @@ import { Typography, Box, Link } from "@mui/material";
 
 const Hero = () => {
 	return (
-		<Box className="w-full py-4 flex gap-4 flex-col">
-			<Typography
-				variant="h1"
-				color="secondary"
-				className="text-6xl md:text-8xl"
-			>
-				OCaRds
-			</Typography>
-			<Typography variant="body1">
-				This is OCaRds, a web app designed to help creating and studying
-				flashcards. It uses OCR (Optical Character Recognition) and{" "}
-				<Link
-					variant="body1"
-					color="secondary.light"
-					href="https://github.com/tesseract-ocr/tesseract"
-				>
-					tesseract
-				</Link>{" "}
-				library for recognising text. Then it converts them to fully functional
-				flashcards that you can edit and study.
-			</Typography>
-			<div className="h-px w-full bg-white"></div>
+		<Box className="w-full h-screen py-4 flex gap-4">
+			<div className="flex gap-4 flex-col w-1/2">
+				<Typography variant="h3" color="secondary" className="font-semibold">
+					A faster way to create and study flashcards.
+				</Typography>
+				<Typography variant="body1">
+					This is OCaRds, a web app designed to help creating and studying
+					flashcards. It uses OCR (Optical Character Recognition) and{" "}
+					<Link
+						variant="body1"
+						color="secondary.light"
+						href="https://github.com/tesseract-ocr/tesseract"
+					>
+						tesseract
+					</Link>{" "}
+					library for recognising text. Then it converts them to fully
+					functional flashcards that you can edit and study.
+				</Typography>
+			</div>
+			<div className="w-1/2"></div>
 		</Box>
 	);
 };
