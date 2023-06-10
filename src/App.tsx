b1e8165decfc8de6f71d29e30bd40bd9a064a224
TutusBOT <barte77pl@gmail.com>
2023-06-10 23:51:34 +0200

    feat: divide app in two blocks - hero page and sets page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { StyledEngineProvider, ThemeProvider } from "@mui/material";
+import { Button, StyledEngineProvider, ThemeProvider } from "@mui/material";
 import "./App.css";
-import { CreateSet, Hero, SetList, NavBar } from "./components";
+import { CreateSet, Hero, SetList } from "./components";
 import { theme } from "./theme";
 
 function App() {
@@ -8,10 +8,16 @@ function App() {
 		<StyledEngineProvider injectFirst>
 			<ThemeProvider theme={theme}>
 				<div className="w-screen h-screen fixed bg-gray-100 top-0 left-0 -z-50"></div>
-				<NavBar />
 				<Hero />
-				<CreateSet />
-				<SetList />
+				<div className="h-screen w-full py-16" id="cards">
+					<div className="flex justify-center items-center gap-4">
+						<Button variant="outlined" color="secondary" href="#home">
+							Home
+						</Button>
+						<CreateSet />
+					</div>
+					<SetList />
+				</div>
 			</ThemeProvider>
 		</StyledEngineProvider>
 	);
